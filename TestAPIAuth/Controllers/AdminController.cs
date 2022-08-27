598d86d76e1d8c9a1d97430b3521f16dcf8dc352
R. Radu <radu9502@gmail.com>
2022-08-27 21:05:13 +0300

    Added endpoints

---
+++ TestAPIAuth/Controllers/AdminController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TestAPIAuth.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+
+        [HttpGet("Users")]
+        public IEnumerable<string> Users()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("Users/{id}")]
+        public string Users(int id)
+        {
+            return "value";
+        }
+        [HttpDelete("DeleteUser/{id}")]
+        public void DeleteUser(int id)
+        {
+        }
+
+
+
+
+
+        [HttpGet("Categories")]
+        public IEnumerable<string> Categories()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        [HttpGet("Category/{id}")]
+        public string Category(int id)
+        {
+            return "value";
+        }
+
+        [HttpDelete("DeleteCategory/{id}")]
+        public void DeleteCategory(int id)
+        {
+        }
+
+        [HttpGet("SubCategories")]
+        public IEnumerable<string> SubCategories()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        [HttpGet("SubCategories/{id}")]
+        public string SubCategories(int id)
+        {
+            return "value";
+        }
+        [HttpDelete("DeleteSubCategory/{id}")]
+        public void DeleteSubCategory(int id)
+        {
+        }
+
+        [HttpDelete("DeleteRequest/{id}")]
+        public void DeleteRequest(int id)
+        {
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ TestAPIAuth/Controllers/AuthentificationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestAPIAuth.Models;
+
+namespace TestAPIAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthentificationController : ControllerBase
+    {
+
+        // POST api/<Authentification>
+        [HttpPost("Login")]
+        public void Login(string userName, string password)
+        {
+        }
+        [HttpPost("Register")]
+        public void Register(string userName, string password, string email)
+        {
+        }
+    }
+}
