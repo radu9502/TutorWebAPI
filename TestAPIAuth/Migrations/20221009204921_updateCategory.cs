7ff600497dc1730cf4d900df54790bfb5cdeeb78
R. Radu <radu9502@gmail.com>
2022-10-10 00:37:55 +0300

    Updated Category prop

---
+++ TestAPIAuth/Migrations/20221009204921_updateCategory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestAPIAuth.Migrations
+{
+    public partial class updateCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "categories",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "categories");
+        }
+    }
+}
--- TestAPIAuth/Models/Category.cs
+++ TestAPIAuth/Models/Category.cs
@@ -5,6 +5,7 @@
         public int Id { get; private set; }
         public string Name { get; set; }
         public string ImageUrl { get; set; }
+        public string Color { get; set; }  
         public bool Deleted { get; set; }
     }
 }
